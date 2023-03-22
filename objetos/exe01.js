97a2f672b19da76cd4a58713d12fc76cc4da13a7
leocasttro <leocasttro01@icloud.com>
2023-03-22 19:09:45 -0300

    envio

---
+++ objetos/exe01.js
@@ -0,0 +1,18 @@
+const endereco = {
+  rua: 'rua barata ribeiro',
+  numero: '23',
+  complemento: 'sem complemento',
+  bairro: 'Copacabana',
+  cep: '72630-122',
+  cidade: 'Rio de janeiro',
+  estado: 'Rio de Janeiro',
+  pais: 'Brasil'
+}
+
+const usuario = {
+  nome: 'Leonardo',
+  email: 'leonardo@email.com',
+  telefone: '61998918274',
+  data_nascimento: '10/04/1995',
+  endereco: endereco
+}
